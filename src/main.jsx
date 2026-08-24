708fde6121cbe178bbbc61287a11140504402b38
dogandgn <doganaric@gmail.com>
2026-08-25 00:10:57 +0300

    Vercel Analytics eklendi

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './components/styles/globals.css'
 import Index from './components/pages/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Index />
+    <Analytics />
   </React.StrictMode>,
 )
